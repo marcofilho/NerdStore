7ede71266de539104f51b0c2b07491e3edb76967
Marco Filho <m.barceloslima@gmail.com>
2025-12-03 11:54:38 -0800

    Add project files.

---
+++ src/NerdStore.Catalog.Domain/Product.cs
@@ -0,0 +1,97 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Product : Entity, IAggregateRoot
+    {
+        public Guid CategoryId { get; private set; }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Price { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string Image { get; private set; }
+        public int StockAmount { get; private set; }
+
+        public Category Category { get; private set; }
+
+        public Product(string name, string description, bool active, decimal price, Guid categoryId, DateTime createdAt, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Price = price;
+            CategoryId = categoryId;
+            CreatedAt = createdAt;
+            Image = image;
+
+            Validate();
+        }
+
+        public void Activate() => Active = true;
+
+        public void Deactivate() => Active = false;
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = Category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Validation.ValidateIfEmpty(Description, "The field Description can't be empty.");
+            Description = description;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount < 0) amount *= -1;
+
+            if (!StockAvailable(amount)) throw new DomainException("Unavailable Stock Amount.");
+
+            StockAmount -= amount;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            if (amount < 0) amount *= -1;
+
+            StockAmount += amount;
+        }
+
+        public bool StockAvailable(int amount)
+        {
+            return StockAmount >= amount;
+        }
+
+        public void Validate()
+        {
+            Validation.ValidateIfEmpty(Name, "The field Name can't be empty.");
+            Validation.ValidateIfEmpty(Description, "The field Description can't be empty.");
+            Validation.ValidateIfDifferent(CategoryId, Guid.Empty, "The field CategoryID can't be empty.");
+            Validation.ValidateIfLessThanMinimum(Price, 0, "The field Price of the Product can't be less or equals 0.");
+            Validation.ValidateIfEmpty(Image, "The field Imag can't be empty.");
+        }
+
+    }
+
+    public class Category : Entity
+    {
+        public string Name { get; private set; }
+        public string Code { get; private set; }
+
+        public Category(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/Validation.cs
@@ -0,0 +1,157 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class Validation
+    {
+        public static void ValidateIfEquals(object firstObject, object secondObject, string message)
+        {
+            if (!firstObject.Equals(secondObject))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfDifferent(object firstObject, object secondObject, string message)
+        {
+            if (firstObject.Equals(secondObject))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateCaracters(string value, int maximum, string message)
+        {
+            var length = value.Trim().Length;
+
+            if (length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateCaracters(string value, int minimum, int maximum, string message)
+        {
+            var length = value.Trim().Length;
+
+            if (length < minimum || length > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateExpression(string pattern, string value, string message)
+        {
+            var regex = new Regex(pattern);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfEmpty(string value, string message)
+        {
+            if (value is null || value.Trim().Length == 0)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMininumMaximum(double amount, double minimum, double maximum, string message)
+        {
+            if (amount < minimum || amount > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMininumMaximum(float amount, float minimum, float maximum, string message)
+        {
+            if (amount < minimum || amount > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMininumMaximum(int amount, int minimum, int maximum, string message)
+        {
+            if (amount < minimum || amount > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMininumMaximum(long amount, long minimum, long maximum, string message)
+        {
+            if (amount < minimum || amount > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateMininumMaximum(decimal amount, decimal minimum, decimal maximum, string message)
+        {
+            if (amount < minimum || amount > maximum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThanMinimum(long amount, long minimum, string message)
+        {
+            if (amount <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThanMinimum(decimal amount, decimal minimum, string message)
+        {
+            if (amount <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThanMinimum(int amount, int minimum, string message)
+        {
+            if (amount <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfLessThanMinimum(float amount, float minimum, string message)
+        {
+            if (amount <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+        public static void ValidateIfLessThanMinimum(double amount, double minimum, string message)
+        {
+            if (amount <= minimum)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfFalse(bool expression, string message)
+        {
+
+            if (expression)
+            {
+                throw new DomainException(message);
+            }
+        }
+
+        public static void ValidateIfTrue(bool expression, string message)
+        {
+            if (!expression)
+            {
+                throw new DomainException(message);
+            }
+        }
+    }
+}
